8d1a30992bb8b2c6b6a299c49aa7ba7215875343
imbarteq <97965298+imbarteq@users.noreply.github.com>
2022-05-10 09:49:55 +0200

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -3,10 +3,10 @@
 # Author            : Bartosz Bartczak (s188848@student.pg.edu.pl)
 # Created on        : 25.04.2022
 # Last Modified By  : Bartosz Bartczak (s188848@student.pg.edu.pl)
-# Last Modified On  : 06.05.2022
-# Version           : v0.3
+# Last Modified On  : 10.05.2022
+# Version           : v0.4
 #
-# Description       : ...
+# Description       : Program do tworzenia i odtwarzania kopii zapasowej
 # Opis
 #
 # Licensed under GPL (see /usr/share/common-licenses/GPL for more details
@@ -43,7 +43,9 @@ while getopts ":hv" opt; do
 
 while true; do
 
-    MENU_CHOICE="$(zenity --width=500 --height=300 --list --column " " --title="Backup commander" \
+    LAST_BACKUP=$(<config.txt)
+
+    MENU_CHOICE="$(zenity --width=500 --height=300 --list --column " " --title="Backup commander" --text="Last backup date   :   $LAST_BACKUP" \
     "Stworz backup" \
     "Odtworz backup" \
     "Zaplanuj backup" \
@@ -51,7 +53,7 @@ while true; do
 
     if [[ $MENU_CHOICE == "Stworz backup" ]] ; then
 
-        BACKUP_INFO=$(zenity --forms --title="Backup commander" --text="Please enter needed information" --separator=";" --add-entry="Katalog do backupu" --add-entry="Katalog docelowy")
+        BACKUP_INFO=$(zenity --forms --title="Backup commander" --text="Podaj informacje do backupu" --separator=";" --add-entry="Katalog do backupu" --add-entry="Katalog docelowy")
 
         FOLDER=$(echo $BACKUP_INFO | cut -d ';' -f 1)
         DESTINATION=$(echo $BACKUP_INFO | cut -d ';' -f 2)
@@ -69,6 +71,8 @@ while true; do
             cp $DATE.tar $DESTINATION
             rm $DATE.tar
             zenity --info --width=300 --height=50 --text="Kopia zapasowa zostala pomyslnie utworzona"
+
+            echo $DATE > config.txt
         fi
 
     fi
@@ -84,7 +88,7 @@ while true; do
 
         FILENAME="$(basename -- $FILES)"
 
-        cp $FILES $DESTINATION
+        cp -i $FILES $DESTINATION
         cd $DESTINATION
         tar -xvf $FILES
         rm $FILENAME
@@ -103,7 +107,47 @@ while true; do
 
     if [[ $MENU_CHOICE == "Zaplanuj backup" ]] ; then
 
-        PLANNED_BACKUP_INFO=$(zenity --forms --title="Backup commander" --text="Please enter needed information" --separator=";" --add-entry="Katalog do backupu" --add-entry="Katalog docelowy" --add-entry="Godzina []")
+        PLANNED_BACKUP_INFO=$(zenity --forms --title="Backup commander" --text="Please enter needed information" --separator=";" --add-entry="Katalog do backupu" --add-entry="Katalog docelowy" --add-entry="Co ile? [sekund]" --add-entry="Ile razy?")
+
+        FOLDER=$(echo $PLANNED_BACKUP_INFO | cut -d ';' -f 1)
+        DESTINATION=$(echo $PLANNED_BACKUP_INFO | cut -d ';' -f 2)
+        TIME=$(echo $PLANNED_BACKUP_INFO | cut -d ';' -f 3)
+        REPEAT=$(echo $PLANNED_BACKUP_INFO | cut -d ';' -f 4)
+
+        COUNT=0
+
+        if [[ $FOLDER == "" ]] ; then
+            zenity --width=300 --height=50 --error --text="Nie wybrano folderu bazowego"
+        else
+
+            if [[ $DESTINATION == "" ]] ; then
+                zenity --width=300 --height=50 --error --text="Nie wybrano folderu docelowego"
+            else
+
+                if [[ $TIME == "" ]] ; then
+                    TIME=0
+                fi
+
+                if [[ $REPEAT == "" ]] ; then
+                    REPEAT=1
+                fi
+
+                zenity --info --width=300 --height=50 --text="Kopia zapasowa zostala pomyslnie zaplanowana"
+
+                while [[ $COUNT != $REPEAT ]] ; do
+                    DATE=$(date +%Y-%m-%d_%H-%M-%S)
+                    sleep $TIME
+                    DEST_FOLER=$DESTINATION
+                    tar -cvf $DATE.tar -C $FOLDER .
+                    cp $DATE.tar $DESTINATION
+                    rm $DATE.tar
+                    echo $DATE > config.txt
+                    COUNT=$(($COUNT+1))
+                done
+
+            fi
+
+        fi
 
     fi
 
